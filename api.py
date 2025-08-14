37b58a62ce755f5e37987da9158e6f6b58aacdb4
Alex S <jorgesanchez0228@gmail.com>
2025-08-14 15:19:40 -0400

    first commit: gui and movie class to access themoviedb database and api and graphical interface
    * add gui functionality to add title, release date, overview, and rating to gui dynamically

---
+++ api.py
@@ -0,0 +1,125 @@
+import requests
+import random
+
+# const of user token and api url
+API_URL = "https://api.themoviedb.org/3/authentication"
+HEADERS = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzIyOWZiYmE3YzNkZDlkZmI1YzA0NzA3ZmYxMjUwNyIsIm5iZiI6MTc1NTE3Mzk4Ny40Miwic3ViIjoiNjg5ZGQ0NjNkN2UxMjU2YjQwYzcwYWFkIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.OXfouADxFlXHaLV6y7KQ7kF15pNzkpZTEHl8nag3L-Q"
+}
+
+# class Movies to handle api token confirmation, popular movies
+
+
+class Movies:
+    def __init__(self):
+        self.confirm_api = requests.get(url=API_URL, headers=HEADERS)
+        self.headers = {
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzIyOWZiYmE3YzNkZDlkZmI1YzA0NzA3ZmYxMjUwNyIsIm5iZiI6MTc1NTE3Mzk4Ny40Miwic3ViIjoiNjg5ZGQ0NjNkN2UxMjU2YjQwYzcwYWFkIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.OXfouADxFlXHaLV6y7KQ7kF15pNzkpZTEHl8nag3L-Q"
+        }
+        self.movies = []
+        self.actors = []
+    # ***FUNCTIONS BELOW ARE FOR FUTURE TRIVIA GAME IMPLEMENTATION***
+    # confirm connection to api with token
+
+    def confirm(self):
+        return self.confirm_api.json()
+
+    # return a list of popular movies
+    def popular(self):
+        url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+
+        response = requests.get(url=url, headers=self.headers)
+        return response.json() if response.status_code == 200 else print("Error:", response.status_code, response.text)
+    # return a list of the most popular movies for given pages
+
+    def top_rated(self):
+        # 20 per page, 10 pages, 200 movies
+        for page in range(1, 51):
+            url = f"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page={page}"
+            response = requests.get(url=url, headers=self.headers)
+            if response.status_code == 200:
+                data = response.json()
+                self.movies.extend(data["results"])
+            else:
+                print("Error:", response.status_code, response.text)
+
+    # actor information for later implementation
+    # uses custom_score to attempt to get popular actors of all time
+    def get_actors(self, pages, threshold, vote_weight, pop_weight):
+        for page in range(1, pages + 1):
+            url = f"https://api.themoviedb.org/3/person/popular?&page={page}"
+            response = requests.get(url=url, headers=self.headers)
+            if response.status_code != 200:
+                print("Error")
+                continue
+
+            data = response.json()
+
+            for actor in data["results"]:
+
+                if actor.get("known_for_department") != "Acting":
+                    continue
+
+                known_for = actor.get("known_for", [])
+                vote_avgs = [item.get("vote_average", 0) for item in known_for if item.get(
+                    "vote_average") is not None]
+
+                if vote_avgs:
+                    avg_vote = round(sum(vote_avgs) /
+                                     len(vote_avgs), ndigits=2)
+                else:
+                    avg_vote = 0
+
+                tmdb_pop = actor.get("popularity", 0)
+                custom_score = round(
+                    vote_weight * avg_vote + pop_weight * tmdb_pop, 2)
+
+                if custom_score >= threshold:
+                    actor_entry = {
+                        "name": actor["name"],
+                        "custom_score": custom_score,
+                        "known_for": [item["title"] if item["media_type"] == "movie" else item["name"] for item in known_for]
+                    }
+                    self.actors.append(actor_entry)
+
+    # movie trivia for later implementation
+    def trivia_game(self):
+        '''while True:
+            movie_field = random.choice(list(random_movie.keys()))
+            print(movie_field.title())
+            print(random_movie[movie_field])
+
+            choice = input(
+                "Guess the movie title, get another hint (any key), or exit ('0')? >> ")
+
+            if (choice == "0"):
+                break
+            if (choice.lower() == random_movie["title"].lower()):
+                print(f"That's right, the film is {random_movie['title']}")
+
+            random_movie.pop(movie_field)'''
+    # ***FUNCTIONS ABOVE ARE FOR FUTURE TRIVIA GAME IMPLEMENTATION***
+    # ***FUNCTIONS BELOW ARE FOR MOVIE RECC IMPLEMENTATION***
+
+    def search(self, name: str):
+        url = f"https://api.themoviedb.org/3/search/movie?query={name}"
+        response = requests.get(url=url, headers=self.headers)
+        return response.json()["results"]
+
+
+if __name__ == "__main__":
+    api = Movies()
+    # COMMENTS BELOW ARE FOR TRIVIA FUTURE IMPLEMENTATION
+    '''
+    print("Loading actors...")
+    api.get_actors(pages=200, threshold=7.0, pop_weight=0.3, vote_weight=0.7)
+    actors = api.actors
+    print("Loading movies...")
+    api.top_rated()
+    movies = api.movies
+
+    for movie in movies:
+        print(movie["title"])
+    '''
---
+++ gui.py
@@ -0,0 +1,55 @@
+import customtkinter
+from api import Movies
+
+
+class App(customtkinter.CTk):
+    def __init__(self) -> None:
+        super().__init__()
+        self.geometry("350x500")
+        self.grid_columnconfigure((0), weight=1)
+
+        self.entry = customtkinter.CTkEntry(self, width=140)
+        self.entry.grid(row=0, column=0, padx=20, pady=5)
+
+        self.button = customtkinter.CTkButton(
+            self, text="Search", command=self.button_event, width=70)
+        self.button.grid(row=1, column=0, pady=3)
+
+        self.title_label = customtkinter.CTkLabel(self, text=None)
+        self.release_label = customtkinter.CTkLabel(self, text=None)
+        self.overview_label = customtkinter.CTkLabel(
+            self, text=None, wraplength=250)
+        self.rating_label = customtkinter.CTkLabel(self, text=None)
+
+        self.title_label.grid(row=2, column=0, pady=5)
+        self.release_label.grid(row=3, column=0, pady=5)
+        self.overview_label.grid(row=4, column=0, pady=5)
+        self.rating_label.grid(row=5, column=0, pady=5)
+
+    def button_event(self):
+        app = Movies()
+        try:
+            movie_name = self.entry.get()
+            movie = app.search(movie_name)
+            first_result = movie[0]
+
+            self.title_label.configure(text=f"Title: {first_result['title']}")
+            self.release_label.configure(
+                text=f"Release Date: {first_result['release_date']}")
+            self.overview_label.configure(
+                text=f"Overview: {first_result['overview']}")
+            self.rating_label.configure(
+                text=f"Rating: {first_result['vote_average']:.2f}")
+
+            print("Search success")
+        except IndexError as e:
+            print(f"Error: {e}")
+            self.title_label.configure(text="Could not find that movie")
+            self.release_label.configure(text=None)
+            self.overview_label.configure(text=None)
+            self.rating_label.configure(text=None)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
